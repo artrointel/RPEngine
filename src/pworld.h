67f59674e9a2952b7284dd1ebd51050434683e34
Woohyun Kim <artrointel@gmail.com>
2015-06-15 18:51:28 +0900

    Create pworld.h

---
+++ src/pworld.h
@@ -0,0 +1,42 @@
+#ifndef _PWORLD_H
+#define _PWORLD_H
+
+#include <vector>
+#include "particle.h"
+#include "pcontacts.h"
+#include "pfgen.h"
+
+namespace RPEngine
+{
+	class ParticleWorld
+	{
+	public:
+		typedef std::vector<Particle *> Particles;
+		typedef std::vector<ParticleContactGenerator *> PContactGenerators;
+
+	public:
+		Particles particles;
+		PContactGenerators contactGenerators;
+		
+		ParticleForceRegistry registry;
+		ParticleContactResolver resolver;
+		bool calculateIterations; // true if resolver should calculate at that frame
+
+		ParticleContact *contacts;
+		unsigned int maxContacts;
+
+	public:
+		explicit ParticleWorld(unsigned int _maxContacts, unsigned int iterations = 0);
+		virtual ~ParticleWorld();
+	public:
+		void startFrame(); // clear all particle's forces
+		unsigned int generateContacts();
+		void integrate(real duration);
+		void runPhysics(real duration);
+
+	};
+
+}
+
+
+#endif
